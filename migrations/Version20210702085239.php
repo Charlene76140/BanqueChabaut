5d21fcabe6d484974e83baae87e679cc0c827657
Thibaut <thibaut-jean@live.fr>
2021-07-02 16:07:09 +0200

    On se remet à jourgit add -A!

--- migrations/Version20210702085239.php
+++ migrations/Version20210702085239.php
@@ -20,11 +20,11 @@ final class Version20210702085239 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE account (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, number VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, date DATETIME NOT NULL, amount INT NOT NULL, INDEX IDX_7D3656A49D86650F (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE operation (id INT AUTO_INCREMENT NOT NULL, account_id INT NOT NULL, date DATETIME NOT NULL, label VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, amount INT NOT NULL, INDEX IDX_1981A66D49CB4726 (account_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE account (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, number VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, date DATETIME NOT NULL, amount INT NOT NULL, INDEX IDX_7D3656A49D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE operation (id INT AUTO_INCREMENT NOT NULL, account_id_id INT NOT NULL, date DATETIME NOT NULL, label VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, amount INT NOT NULL, INDEX IDX_1981A66D49CB4726 (account_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, postal VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE account ADD CONSTRAINT FK_7D3656A49D86650F FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE operation ADD CONSTRAINT FK_1981A66D49CB4726 FOREIGN KEY (account_id) REFERENCES account (id)');
+        $this->addSql('ALTER TABLE account ADD CONSTRAINT FK_7D3656A49D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE operation ADD CONSTRAINT FK_1981A66D49CB4726 FOREIGN KEY (account_id_id) REFERENCES account (id)');
     }
 
     public function down(Schema $schema): void
