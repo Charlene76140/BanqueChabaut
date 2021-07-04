ddc459b4d7e3ffff421c4d918f783d4174adb0bf
Thibaut <thibaut-jean@live.fr>
2021-07-04 22:52:39 +0200

    Add form for newAccount, need to fix validator, amount min, and ccs button

---
+++ src/Form/NewAccountType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class NewAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('number', null, [
+                "label"=> "Numéro de comte :",
+            ])
+            ->add('type', null, [
+                "label" => "Type de compte"
+            ])
+            ->add('amount', null, [
+                "label" => "Montant"
+            ])
+            ->add('enregistrer', SubmitType::class, [
+                "attr" => ['class' => 'btn bgColorSec text-dark my-3'],
+                'row_attr' => ['class' => 'text-center']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
