18db9ca2fd45d8d0e15e6cb4543f6c98b29ebb4c
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-07-11 18:53:29 +0200

    last update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -19,7 +19,9 @@ class AdminController extends AbstractController
     #[Route('admin/account/{id}', methods:["GET", "POST"], name: 'adminAccount', requirements: ['id' => '\d+'])]
     public function adminAccounts(int $id=1, UserRepository $userRepository): Response
     {
+        //recovery of an account by its id
         $user= $userRepository->find($id);
+        //recovery of all accounts of the logged in user
         $accounts = $user->getAccounts();
 
         return $this->render('admin/account.html.twig', [
